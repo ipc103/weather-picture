743231ba1b243d209390ee26e1b8d0c3ec4ad394
ipc103 <iancandysemail@gmail.com>
2015-11-03 21:26:17 -0500

    add background color

---
+++ app.js
@@ -0,0 +1,31 @@
+
+function preload() {
+
+}
+
+function setup() {
+  var weather;
+  createCanvas(windowWidth, windowHeight);
+  var url = "http://api.wunderground.com/api/563b9b32faee9f40/conditions/q/NY/new_york.json";
+  debugger;
+  $.getJSON(url, function( data ){
+    weather = data.current_observation;
+    setBackgroundColor( weather );
+  })
+}
+
+function draw() {
+
+}
+
+function setBackgroundColor( weather ) {
+  var backgroundColor;
+    if ( weather.temp_f > 80 ) {
+      var backgroundColor = "red";
+    } else if ( weather.temp_f > 70 ) {
+      backgroundColor = "orange";
+    } else if ( weather.temp_f > 60 ) {
+      backgroundColor = "yellow";
+    }
+    background( backgroundColor );
+}
